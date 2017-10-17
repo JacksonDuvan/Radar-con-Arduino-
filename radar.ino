8700feccec452905a460a0dda05f2354d3918fb5
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-10-17 12:02:31 -0500

    Add files via upload

---
+++ radar.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+#include <Ultrasonic.h>
+
+#define TRIG 10
+#define ECHO 11
+
+Servo servo
+Ultrasonic ultrasonic(TRIG, ECHO);
+
+int a=0;
+int dir=5;
+
+void setup(){
+  Serial.begin(9600);
+  servo.attach(9);
+  }
+void loop(){
+  long msec = ultrasonic.timing();
+  float cm = ultrasonic.convert(msec, Ultrasonic::CM);
+
+  a+=dir;
+
+  if(a==0)dir=5;
+  if(a==180)dir=-5;
+
+  delay(100);
+  }
+
